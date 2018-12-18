f6f9590b31826bf94034909a09225e4f39434b2a
Grigorzhevskaya <45313313+grigorgevskaya@users.noreply.github.com>
2018-12-19 00:29:14 +0300

    Add files via upload

--- list.py
+++ list.py
@@ -1,16 +1,6 @@
 import sys
-from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, QTextEdit, QGridLayout, QApplication)
- 
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     w = QWidget()
-#     w.resize(400, 300)
-#     w.move(500, 300)
-#     w.setWindowTitle('Input1')
-#     w.show()
-    
- 
-class Example(QWidget):
+from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, QTextEdit, QGridLayout, QApplication, QPushButton, QHBoxLayout, QVBoxLayout, QDialog)
+class Example(QWidget, QGridLayout):
     
     def __init__(self):
         super().__init__()
@@ -29,15 +19,66 @@ class Example(QWidget):
         grid.addWidget(titleEdit, 1, 1)
         grid.addWidget(author, 2, 0)
         grid.addWidget(authorEdit, 2, 1)
+
+
+        self.setLayout(grid)
+
+        okButton = QPushButton("OK")
+        cancelButton = QPushButton("Cancel")
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(okButton)
+        hbox.addWidget(cancelButton)
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
         
+        self.setLayout(vbox)
+
+        self.login = QLineEdit()
+        self.login.setPlaceholderText('Введите логин...')
+
+        self.password = QLineEdit()
+        self.password.setEchoMode(QLineEdit.Password)
+        self.password.setPlaceholderText('Введите пароль...')
+
+        layout = QFormLayout()
+        layout.addRow('Login:', self.login)
+        layout.addRow('Password:', self.password)
+
+        self.setLayout(layout)
+
+         
+
         
-        self.setLayout(grid) 
-        
-        self.setGeometry(300, 300, 350, 300)
-           
+        self.setGeometry(750, 300, 350, 300)
+        self.setWindowTitle('Авторизация')   
         self.show()
         
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
+
+
+#сlass LoginWindow(QDialog):
+   # def __init__(self):
+        #super().__init__()
+
+        #self.login = QLineEdit()
+        #self.login.setPlaceholderText('Введите логин...')
+
+        #self.password = QLineEdit()
+        #self.password.setEchoMode(QLineEdit.Password)
+        #self.password.setPlaceholderText('Введите пароль...')
+
+        #layout = QFormLayout()
+        #layout.addRow('Login:', self.login)
+        #layout.addRow('Password:', self.password)
+
+        #self.setLayout(layout)
+        
+         
+        
+        
