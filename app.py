5341399c91d5d41bd987b70b12f9505698272f60
deathtospies <litovec.89@gmail.com>
2019-01-15 16:28:03 +0500

    update requirements.txt

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from ui.login import LoginWindow
+from ui.main import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     login = LoginWindow()
+    main_window = MainWindow()
+    main_window.show()
+
     sys.exit(app.exec_())
